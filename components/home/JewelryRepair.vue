c5fcfea2246a104b7223d669811cad0eeb72716f
vue-v1rt24 <rublevrus@mail.ru>
2024-10-09 01:21:33 +0300

    Продолжение того же блока

--- components/home/JewelryRepair.vue
+++ components/home/JewelryRepair.vue
@@ -39,16 +39,17 @@ defineProps<{
             format="avif, webp"
             densities="x1"
             loading="lazy"
-            class="jewelry_repair__list_image"
           />
         </li>
       </ul>
 
       <!--  -->
       <div class="jewelry_repair__podlozhka">
-        <p class="jewelry_repair__podlozhka_text">
-          {{ jewelryRepair.textPodlozhki }}
-        </p>
+        <p
+          v-if="jewelryRepair.textPodlozhki"
+          v-html="jewelryRepair.textPodlozhki"
+          class="jewelry_repair__podlozhka_text"
+        ></p>
       </div>
     </div>
   </div>
@@ -60,7 +61,7 @@ defineProps<{
   background-image: url(/public/images/home/decor_jewelry_repair.svg);
   background-repeat: no-repeat;
   background-position: right top;
-  padding: 120px 160px;
+  padding: 120px 0;
 }
 
 /*  */
@@ -73,5 +74,57 @@ defineProps<{
 .jewelry_repair .p_20 {
   max-width: 492px;
   color: white;
+}
+
+/*  */
+.jewelry_repair__list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin: 42px 0 60px 0;
+}
+
+.jewelry_repair__list_item {
+  position: relative;
+
+  /*  */
+  img {
+    border-radius: 20px;
+  }
+}
+
+.jewelry_repair__list_title {
+  position: absolute;
+  top: 26px;
+  left: 26px;
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 100%;
+  color: var(--main-green);
+  background-color: white;
+  border-radius: 50px;
+  padding: 12px 16px;
+}
+
+/*  */
+.jewelry_repair__podlozhka {
+  background-color: var(--main--sand);
+  border-radius: 20px;
+  padding: 62px;
+}
+
+.jewelry_repair__podlozhka_text {
+  max-width: 785px;
+  font-family: var(--font-family);
+  font-weight: 200;
+  font-size: 24px;
+  line-height: 140%;
+  color: var(--main-green);
+
+  /*  */
+  :global(span) {
+    font-weight: 500;
+  }
 }
 </style>
