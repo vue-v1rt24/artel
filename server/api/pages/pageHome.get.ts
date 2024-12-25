768e45dd7f5b7237aafc2b096fbf77071354fb35
Роман <rublevrus@mail.ru>
2024-12-25 11:24:20 +0300

    Правки

--- server/api/pages/pageHome.get.ts
+++ server/api/pages/pageHome.get.ts
@@ -12,26 +12,26 @@ export default defineEventHandler(async (event) => {
       homeOffSitePodzagolovok: data.data.pageBy.homeContent.homeOffSitePodzagolovok,
       homeOffSiteTekst1: data.data.pageBy.homeContent.homeOffSiteTekst1,
       homeOffSiteTekst2: data.data.pageBy.homeContent.homeOffSiteTekst2,
-      image: data.data.pageBy.homeContent.homeOffSiteIzobrazhenie.node.mediaItemUrl,
+      image: data.data.pageBy.homeContent.homeOffSiteIzobrazhenie.node?.mediaItemUrl,
     },
     catalog: {
       title: data.data.pageBy.homeContent.catalogHomeZagolovok,
       desc: data.data.pageBy.homeContent.catalogHomeTekst,
-      image1600: data.data.pageBy.homeContent.catalogHomeFon1600.node.mediaItemUrl,
-      image1200: data.data.pageBy.homeContent.catalogHomeFon1200.node.mediaItemUrl,
-      image688: data.data.pageBy.homeContent.catalogHomeFon688.node.mediaItemUrl,
-      image320: data.data.pageBy.homeContent.catalogHomeFon320.node.mediaItemUrl,
+      image1600: data.data.pageBy.homeContent.catalogHomeFon1600.node?.mediaItemUrl,
+      image1200: data.data.pageBy.homeContent.catalogHomeFon1200.node?.mediaItemUrl,
+      image688: data.data.pageBy.homeContent.catalogHomeFon688.node?.mediaItemUrl,
+      image320: data.data.pageBy.homeContent.catalogHomeFon320.node?.mediaItemUrl,
     },
     guardians: {
       title: data.data.pageBy.homeContent.guardiansHomeZagolovok,
       desc: data.data.pageBy.homeContent.guardiansHomeOpisanie,
-      image1: data.data.pageBy.homeContent.guardiansHomeIzobrazhenie1.node.mediaItemUrl,
-      image2: data.data.pageBy.homeContent.guardiansHomeIzobrazhenie2.node.mediaItemUrl,
+      image1: data.data.pageBy.homeContent.guardiansHomeIzobrazhenie1.node?.mediaItemUrl,
+      image2: data.data.pageBy.homeContent.guardiansHomeIzobrazhenie2.node?.mediaItemUrl,
     },
     customJewelry: {
       title: data.data.pageBy.homeContent.customJewelryZagolovok,
       desc: data.data.pageBy.homeContent.customJewelryOpisanie,
-      image1600: data.data.pageBy.homeContent.customJewelryIzobrazhenie.node.mediaItemUrl,
+      image1600: data.data.pageBy.homeContent.customJewelryIzobrazhenie.node?.mediaItemUrl,
     },
     jewelryRepair: {
       title: data.data.pageBy.homeContent.jewelryRepairZagolovok,
@@ -42,7 +42,7 @@ export default defineEventHandler(async (event) => {
     lombard: {
       title: data.data.pageBy.homeContent.lombardHomeZagolovok,
       desc: data.data.pageBy.homeContent.lombardHomeTekst,
-      image: data.data.pageBy.homeContent.lombardHomeIzobrazhenie.node.mediaItemUrl,
+      image: data.data.pageBy.homeContent.lombardHomeIzobrazhenie.node?.mediaItemUrl,
       modal: data.data.pageBy.homeContent.lombardHomeRedactor,
     },
   };
